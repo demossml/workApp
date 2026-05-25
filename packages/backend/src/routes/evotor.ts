e516e30365e14ac4314616ffa964f2551d997352
Grisha <grisha@bankrupt.app>
2026-05-25 14:24:35 +0000

    Reduce vape plan groups to 3: ЭЛЕКТРО, ДОН ЭЛЕКТРО, СНС ЭЛЕКТРО

--- packages/backend/src/routes/evotor.ts
+++ packages/backend/src/routes/evotor.ts
@@ -1574,15 +1574,9 @@ export const evotorRoutes = new Hono<IEnv>()
 			await createPlanTable(db);
 
 			const groupIdsVape: string[] = [
-				"78ddfd78-dc52-11e8-b970-ccb0da458b5a",
-				"bc9e7e4c-fdac-11ea-aaf2-2cf05d04be1d",
-				"0627db0b-4e39-11ec-ab27-2cf05d04be1d",
-				"2b8eb6b4-92ea-11ee-ab93-2cf05d04be1d",
-				"8a8fcb5f-9582-11ee-ab93-2cf05d04be1d",
-				"97d6fa81-84b1-11ea-b9bb-70c94e4ebe6a",
-				"ad8afa41-737d-11ea-b9b9-70c94e4ebe6a",
-				"568905bd-9460-11ee-9ef4-be8fe126e7b9",
-				"568905be-9460-11ee-9ef4-be8fe126e7b9",
+				"78ddfd78-dc52-11e8-b970-ccb0da458b5a", // ЭЛЕКТРО
+				"568905bd-9460-11ee-9ef4-be8fe126e7b9", // ДОН ЭЛЕКТРО
+				"568905be-9460-11ee-9ef4-be8fe126e7b9", // СНС ЭЛЕКТРО
 			];
 
 			const plan = await getPlan(datePlan, db);
